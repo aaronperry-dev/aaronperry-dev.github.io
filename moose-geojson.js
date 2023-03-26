240985b29b9906da972db73f32a80c1dbbfa75a3
Aaron Perry <110098800+aaronperry-dev@users.noreply.github.com>
2023-03-26 01:01:41 -0400

    Update moose-geojson.js

--- moose-geojson.js
+++ moose-geojson.js
@@ -1,91 +1,42 @@
 var mooseSightings = {
     "type": "FeatureCollection",
-    "name": "2022_nh_moose",
-    "crs": {
-        "type": "name",
-        "properties": {
-            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
-        }
-    },
     "features": [{
-            "type": "Feature",
-            "properties": {
-                "lat": 44.296764879302607,
-                "lon": -71.516738332198997,
-                "moose_sighting": "1"
-            },
             "geometry": {
                 "type": "Point",
                 "coordinates": [-71.516738332198997, 44.296764879302607]
             }
         },
         {
-            "type": "Feature",
-            "properties": {
-                "lat": 43.291967642142176,
-                "lon": -71.826363535175787,
-                "moose_sighting": "2"
-            },
             "geometry": {
                 "type": "Point",
                 "coordinates": [-71.826363535175787, 43.291967642142176]
             }
         },
         {
-            "type": "Feature",
-            "properties": {
-                "lat": 43.864122928898254,
-                "lon": -71.212112044541243,
-                "moose_sighting": "3"
-            },
             "geometry": {
                 "type": "Point",
                 "coordinates": [-71.212112044541243, 43.864122928898254]
             }
         },
         {
-            "type": "Feature",
-            "properties": {
-                "lat": 44.489851483282791,
-                "lon": -71.571724527895853,
-                "moose_sighting": "4"
-            },
             "geometry": {
                 "type": "Point",
                 "coordinates": [-71.571724527895853, 44.489851483282791]
             }
         },
         {
-            "type": "Feature",
-            "properties": {
-                "lat": 44.108007453264094,
-                "lon": -71.014209531485577,
-                "moose_sighting": "5"
-            },
             "geometry": {
                 "type": "Point",
                 "coordinates": [-71.014209531485577, 44.108007453264094]
             }
         },
         {
-            "type": "Feature",
-            "properties": {
-                "lat": 44.394851689887076,
-                "lon": -71.468975317820494,
-                "moose_sighting": "6"
-            },
             "geometry": {
                 "type": "Point",
                 "coordinates": [-71.468975317820494, 44.394851689887076]
             }
         },
         {
-            "type": "Feature",
-            "properties": {
-                "lat": 43.726665056756964,
-                "lon": -71.390534727798311,
-                "moose_sighting": "7"
-            },
             "geometry": {
                 "type": "Point",
                 "coordinates": [-71.390534727798311, 43.726665056756964]
