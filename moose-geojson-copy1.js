c35fa4c4427302d931586071f2ade3146a6b3d8f
Aaron Perry <110098800+aaronperry-dev@users.noreply.github.com>
2023-03-26 01:22:32 -0400

    Add files via upload

---
+++ moose-geojson-copy1.js
@@ -0,0 +1,10 @@
+var coorsField = {
+    "type": "Feature",
+    "properties": {
+        "popupContent": "Coors Field"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-71.516738332198997, 44.296764879302607]
+    }
+;}
